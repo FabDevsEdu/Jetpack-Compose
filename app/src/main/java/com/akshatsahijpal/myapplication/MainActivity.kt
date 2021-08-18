9ee6977364bc03bc0bc901ffe7b3a8be53f78bfc
Ghost <20akshatsahijpal01droid01@gmail.com>
2021-08-18 08:30:46 +0530

    v1

--- app/src/main/java/com/akshatsahijpal/myapplication/MainActivity.kt
+++ app/src/main/java/com/akshatsahijpal/myapplication/MainActivity.kt
@@ -3,32 +3,42 @@ package com.akshatsahijpal.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.akshatsahijpal.myapplication.ui.models.MainViewModel
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            getContentStateful()
         }
     }
-}
-@Preview
-@Composable
-fun prev() {
-    LazyColumn{
-        itemsIndexed(listOf("a", "b", "c")){ index, item -> }
-        items(400){
-            Text(text = "for $it")
+    @Composable
+    fun getContentStateful() {
+        var ix by rememberSaveable {
+            mutableStateOf("")
+        }
+        getContentStateless(ix) {
+            ix = it
+        }
+    }
+    @Composable
+    fun getContentStateless(
+        cv: String,
+        changeCv: (String) -> Unit)
+    {
+        Column(modifier = Modifier.fillMaxSize(0.5f)) {
+            Text(text = "Current Version = $cv")
+            Spacer(modifier = Modifier.height(50.dp))
+            OutlinedTextField(value = cv, onValueChange = changeCv, label = { Text(text = "CV")})
         }
     }
 }
--- app/src/main/java/com/akshatsahijpal/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/akshatsahijpal/myapplication/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.akshatsahijpal.myapplication.ui.theme
+ package com.akshatsahijpal.myapplication.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
