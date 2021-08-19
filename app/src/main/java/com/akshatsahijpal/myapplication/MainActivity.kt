67c228e8f072087921b1e6d84c68a0e4e2907499
Ghost <20akshatsahijpal01droid01@gmail.com>
2021-08-19 08:03:53 +0530

    v1

--- app/src/main/java/com/akshatsahijpal/myapplication/MainActivity.kt
+++ app/src/main/java/com/akshatsahijpal/myapplication/MainActivity.kt
@@ -2,43 +2,41 @@ package com.akshatsahijpal.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.akshatsahijpal.myapplication.ui.models.MainViewModel
-import java.util.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity(enabled: Boolean) : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            getContentStateful()
+
         }
     }
+
     @Composable
-    fun getContentStateful() {
-        var ix by rememberSaveable {
-            mutableStateOf("")
-        }
-        getContentStateless(ix) {
-            ix = it
-        }
-    }
-    @Composable
-    fun getContentStateless(
-        cv: String,
-        changeCv: (String) -> Unit)
-    {
-        Column(modifier = Modifier.fillMaxSize(0.5f)) {
-            Text(text = "Current Version = $cv")
-            Spacer(modifier = Modifier.height(50.dp))
-            OutlinedTextField(value = cv, onValueChange = changeCv, label = { Text(text = "CV")})
+    fun ErrorBar() {
+        var scaffoldState = rememberScaffoldState()
+        Scaffold(scaffoldState = scaffoldState, modifier = Modifier.fillMaxSize(0.5f)) {
+            var counter:Int by remember{
+                mutableStateOf(0)
+            }
+            LaunchedEffect(key1 = scaffoldState.snackbarHostState){
+                scaffoldState.snackbarHostState.showSnackbar("Counting for $counter")
+            }
+            Button(onClick = {
+                counter++
+            }) {
+               Text(text = "Current Value :${counter}")
+            }
         }
     }
 }
---
+++ app/src/main/java/com/akshatsahijpal/myapplication/ui/fragment/MainFragment.kt
@@ -0,0 +1,10 @@
+package com.akshatsahijpal.myapplication.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
